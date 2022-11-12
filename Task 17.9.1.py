1a13860eb76e26456f66d82051a0867bc5a48b76
TyaminViktor <112327361+TyaminViktor@users.noreply.github.com>
2022-11-12 12:25:31 +0300

    Task_17.9
    First commited

---
+++ Task 17.9.1.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def checking(any_str, any_number):
+    any_str = any_str.split()
+    try:
+        for i in range(len(any_str)):
+            float(any_str[i])
+        float(any_number)
+    except TypeError:
+        print("Ошибка. Значения введены некорректно. Закрытие программы.")
+        sys.exit()
+
+
+def changing(any_str):
+    listed = []
+    any_str = any_str.split()
+    for i in range(len(any_str)):
+        listed.append(float(any_str[i]))
+    return listed
+
+
+def sorting(any_list):
+    for i in range(1, len(any_list)):
+        index = i
+        a = any_list[i]
+        while index > 0 and any_list[index-1] > a:
+            any_list[index] = any_list[index-1]
+            index -= 1
+        any_list[index] = a
+    return any_list
+
+
+def comparing(any_list, any_number):
+    any_number = float(any_number)
+    for i in range(1, len(any_list)):
+        if any_list[i] >= any_number:
+            position = i - 1
+            break
+    else:
+        position = len(any_list)
+    print(f"Ближайшая позиция в последовательности - {position + 1}")
+
+
+some_str = input("Введите последовательность чисел, отделяя каждое последующее одним пробелом: ")
+some_number = input("Введите число, относительная позиция которого в последовательности будет определена: ")
+
+checking(some_str, some_number)
+
+comparing(sorting(changing(some_str)), some_number)
